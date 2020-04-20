e77ae80409bf400fb8575628337a5d72b584e4d7
Ricardo Cesar Rodrigues <62864640+rickkk856@users.noreply.github.com>
2020-04-19 22:02:58 -0300

    Update runway_model.py

--- runway_model.py
+++ runway_model.py
@@ -22,63 +22,31 @@
 
 # =========================================================================
 
-# This example contains the minimum specifications and requirements
-# to port a machine learning model to Runway.
-
-# For more instructions on how to port a model to Runway, see the Runway Model
-# SDK docs at https://sdk.runwayml.com
-
-# RUNWAY
-# www.runwayml.com
-# hello@runwayml.com
-
-# =========================================================================
-
-# Import the Runway SDK. Please install it first with
-# `pip install runway-python`.
 import runway
-from runway.data_types import number, text, image
-from example_model import ExampleModel
+from runway.data_types import category, vector, image
+from your_code import model
 
-# Setup the model, initialize weights, set the configs of the model, etc.
-# Every model will have a different set of configurations and requirements.
-# Check https://docs.runwayapp.ai/#/python-sdk to see a complete list of
-# supported configs. The setup function should return the model ready to be
-# used.
-setup_options = {
-    'truncation': number(min=1, max=10, step=1, default=5, description='Example input.'),
-    'seed': number(min=0, max=1000000, description='A seed used to initialize the model.')
-}
-@runway.setup(options=setup_options)
+options = {"network_size": category(choices=[64, 128, 256, 512], default=256)}
+@runway.setup(options=options)
 def setup(opts):
-    msg = '[SETUP] Ran with options: seed = {}, truncation = {}'
-    print(msg.format(opts['seed'], opts['truncation']))
-    model = ExampleModel(opts)
-    return model
+    return model(network_size=opts["network_size"])
 
-# Every model needs to have at least one command. Every command allows to send
-# inputs and process outputs. To see a complete list of supported inputs and
-# outputs data types: https://sdk.runwayml.com/en/latest/data_types.html
-@runway.command(name='generate',
-                inputs={ 'caption': text() },
-                outputs={ 'image': image(width=512, height=512) },
-                description='Generates a red square when the input text input is "red".')
-def generate(model, args):
-    print('[GENERATE] Ran with caption value "{}"'.format(args['caption']))
-    # Generate a PIL or Numpy image based on the input caption, and return it
-    output_image = model.run_on_input(args['caption'])
-    return {
-        'image': output_image
-    }
 
-if __name__ == '__main__':
-    # run the model server using the default network interface and ports,
-    # displayed here for convenience
-    runway.run(host='0.0.0.0', port=8000)
+sample_inputs= {
+    "z": vector(length=512),
+    "category": category(choices=["day", "night"])
+}
 
-## Now that the model is running, open a new terminal and give it a command to
-## generate an image. It will respond with a base64 encoded URI
-# curl \
-#   -H "content-type: application/json" \
-#   -d '{ "caption": "red" }' \
-#   localhost:8000/generate
+sample_outputs = {
+    "image": image(width=1024, height=1024)
+}
+
+@runway.command("sample", inputs=sample_inputs, outputs=sample_outputs)
+def sample(model, inputs):
+    img = model.sample(z=inputs["z"], category=inputs["category"])
+    # `img` can be a PIL or numpy image. It will be encoded as a base64 URI
+    # string automatically by @runway.command().
+    return { "image": img }
+
+if __name__ == "__main__":
+    runway.run()
